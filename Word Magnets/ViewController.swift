4c825373bbcfe9f551f767a6eeb0467fa1787e8b
Chris Archibald <carchi8py@gmail.com>
2015-09-20 12:39:00 -0700

    Initial check in from playground.

--- Word Magnets/ViewController.swift
+++ Word Magnets/ViewController.swift
@@ -13,6 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        var wordArray = ["I", "like", "to", "eat", "pizza", "my", "favorite"]
+        
+        for word in wordArray {
+            var label = UILabel()
+            label.text = word
+            label.font = UIFont.systemFontOfSize(32)
+            
+            label.sizeToFit()
+            
+            label.backgroundColor = UIColor.whiteColor()
+            
+            var x = CGFloat(arc4random_uniform(300))
+            var y = CGFloat(arc4random_uniform(450))
+            
+            label.center = CGPoint(x: x, y: y)
+            
+            view.addSubview(label)
+            
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
